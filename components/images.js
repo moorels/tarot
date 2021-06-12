0b78d9f7cf6df3964810b6502504c8112a32f706
moorels <sales@micropls.com>
2021-06-12 10:41:40 +1000

    l1

---
+++ components/images.js
@@ -0,0 +1,15 @@
+
+
+
+const images = [
+    { id: 1, src: '../images/4.jpg' },
+    { id: 2, src: '../images/4.jpg' },
+    { id: 3, src: '../images/4.jpg' },
+    { id: 4, src: '../images/4.jpg' },
+    { id: 5, src: '../images/4.jpg' },
+    
+  ];
+  
+
+
+  export default images;
