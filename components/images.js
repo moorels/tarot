0bf4c572d932ee255f4b7e80fe46df9739ceb689
moorels <sales@micropls.com>
2021-06-15 23:44:07 +1000

    divs

--- components/images.js
+++ components/images.js
@@ -1,15 +1,10 @@
-
 export function Imagestest (){
-
- 
 
 var numm = [];
 while(numm.length < 9){
     var r = Math.floor(Math.random() * 156) + 1;
     if(numm.indexOf(r) === -1) numm.push(r);
 }
-
-
 
     return (numm)
   }
