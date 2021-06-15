db77d67fcff6927d86cf7e265de13911603039a2
moorels <sales@micropls.com>
2021-06-15 21:28:13 +1000

    with delay

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,16 @@ numm.push(x)
 }
 console.log(numm)
  
+const [show, setShow] = React.useState(false)
 
+
+React.useEffect(() => {
+    setTimeout(() => {
+      setShow(true)
+    }, 2000)
+  }, [show])
+
+  if (!show) return null
 
   return (
     <div >
